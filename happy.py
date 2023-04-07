9e8b84469d06b21283db3a3ffb4b5822a14a412f
unknown <masurkarvibhuti134@gmail.com>
2023-04-07 22:20:19 +0530

    thats the achievement

--- happy.py
+++ happy.py
@@ -1,1 +1,20 @@
-print("i am enyoing my life")
+a=[]
+def counting(arg,s=""):
+    v=len(a)
+    for i in range(len(arg)):
+        count=0
+        if arg[i]!=s:
+            for j in range(i,len(arg)):
+                if arg[i]==arg[j]:
+                    s=arg[i]
+                    count+=1
+                else:
+                    break
+        if count!=0:
+            a.extend([arg[i],count])
+    return a[v:]           
+b=[1]
+for i in range(6):
+    print(b)
+    b=counting(b)
+    
